cef37a16ca79d9239d4deb862338e33ec22b773c
Matias Fariña <farinamatias@gmail.com>
2025-09-11 15:03:51 -0300

    estable

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -86,6 +86,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static & media
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"   # <- NUEVO
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
