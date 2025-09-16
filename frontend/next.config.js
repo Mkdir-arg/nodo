a6218636286aa0e7945881ed3a859c1b7322d251
Matias Fariña <farinamatias@gmail.com>
2025-09-15 22:35:27 -0300

    delete

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    turbo: {},
+  },
 };
 
 module.exports = nextConfig;
